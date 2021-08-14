4cb92dea5175afdcc05a6ef4992dcc799d212330
Alex Freire <alexfreirespinola@gmail.com>
2021-08-14 15:24:04 -0300

    test: improve carousel card component tests

--- src/components/__tests__/CarouselCard.test.js
+++ src/components/__tests__/CarouselCard.test.js
@@ -1,14 +1,44 @@
 import 'react-native';
 import React from 'react';
-import { render } from '../../utils/Tests';
+import { render, fireEvent } from '../../utils/Tests';
 import CarouselCard, { hints } from '../CarouselCard';
+
+const movie = {
+  backdrop:
+    'https://wookie.codesubmit.io/static/backdrops/d6822b7b-48bb-4b78-ad5e-9ba04c517ec8.jpg',
+  cast: ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart'],
+  classification: '13+',
+  director: 'Christopher Nolan',
+  genres: ['Action', 'Crime', 'Drama'],
+  id: 'd6822b7b-48bb-4b78-ad5e-9ba04c517ec8',
+  imdb_rating: 9.0,
+  length: '2h 32min',
+  overview:
+    'Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.',
+  poster:
+    'https://wookie.codesubmit.io/static/posters/d6822b7b-48bb-4b78-ad5e-9ba04c517ec8.jpg',
+  released_on: '2008-07-16T00:00:00',
+  slug: 'the-dark-knight-2008',
+  title: 'The Dark Knight',
+};
 
 describe('<CarouselCard />', () => {
   it('should render component properly', () => {
-    const imageUri =
-      'https://wookie.codesubmit.io/static/posters/d6822b7b-48bb-4b78-ad5e-9ba04c517ec8.jpg';
-    const { getByA11yHint } = render(<CarouselCard imageUri={imageUri} />);
+    const onMovieClickFn = jest.fn();
+    const { getByA11yHint, getByTestId } = render(
+      <CarouselCard
+        movie={movie}
+        onMovieClick={onMovieClickFn}
+        testID="card-container"
+      />
+    );
     const image = getByA11yHint(hints.cardImage);
-    expect(image).toHaveProp('source', { uri: imageUri });
+    const cardContainer = getByTestId('card-container');
+
+    fireEvent(cardContainer, 'onPress');
+    expect(onMovieClickFn).toHaveBeenCalled();
+    expect(onMovieClickFn).toHaveBeenCalledTimes(1);
+    expect(onMovieClickFn).toHaveBeenCalledWith(movie);
+    expect(image).toHaveProp('source', { uri: movie.poster });
   });
 });
