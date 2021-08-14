6350d4d4712f5f43f2bc3aa29b2e2619b4f56e4e
Alex Freire <alexfreirespinola@gmail.com>
2021-08-14 15:22:06 -0300

    feat: install new font package and add it app theme

--- App.tsx
+++ App.tsx
@@ -3,12 +3,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { ThemeProvider } from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Barlow_100Thin,
+  Barlow_200ExtraLight,
+  Barlow_300Light,
+  Barlow_400Regular,
+  Barlow_500Medium,
+  Barlow_600SemiBold,
+  Barlow_700Bold,
+  Barlow_800ExtraBold,
+  Barlow_900Black,
+} from '@expo-google-fonts/barlow';
 
 import Theme from './src/config/Theme';
 import HomePage from './src/pages/Home';
 
 export type TabsParamList = {
   Home: undefined;
+  Search: undefined;
 };
 
 const Tab = createBottomTabNavigator<TabsParamList>();
@@ -16,9 +31,25 @@ const Tab = createBottomTabNavigator<TabsParamList>();
 const routeIcons = {
   Home: 'home-outline',
   HomeFocused: 'home',
+  Search: 'search-outline',
+  SearchFocused: 'search',
 };
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Barlow_100Thin,
+    Barlow_200ExtraLight,
+    Barlow_300Light,
+    Barlow_400Regular,
+    Barlow_500Medium,
+    Barlow_600SemiBold,
+    Barlow_700Bold,
+    Barlow_800ExtraBold,
+    Barlow_900Black,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <ThemeProvider theme={Theme}>
       <NavigationContainer>
@@ -29,8 +60,16 @@ export default function App() {
                 routeIcons[focused ? `${route.name}Focused` : `${route.name}`];
               return <Ionicons name={iconName} size={size} color={color} />;
             },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
+            tabBarActiveTintColor: '#FFF',
+            tabBarInactiveTintColor: '#363740',
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: '#1c1c26',
+            },
+            tabBarShowLabel: false,
+            tabBarBadgeStyle: {
+              backgroundColor: 'red',
+            },
           })}
         >
           <Tab.Screen name="Home" component={HomePage} />
--- src/config/Theme.ts
+++ src/config/Theme.ts
@@ -1,3 +1,17 @@
+const fonts = {
+  Barlow_100Thin: 'Barlow_100Thin',
+  Barlow_200ExtraLight: 'Barlow_200ExtraLight',
+  Barlow_300Light: 'Barlow_300Light',
+  Barlow_400Regular: 'Barlow_400Regular',
+  Barlow_500Medium: 'Barlow_500Medium',
+  Barlow_600SemiBold: 'Barlow_600SemiBold',
+  Barlow_700Bold: 'Barlow_700Bold',
+  Barlow_800ExtraBold: 'Barlow_800ExtraBold',
+  Barlow_900Black: 'Barlow_900Black',
+};
+
 export default {
-  backgroundColor: '#29282C',
+  ...fonts,
+  backgroundColor: '#1c1c26',
+  lightGrey: '#2b2c33',
 };
