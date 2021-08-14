7d1d906c83ba37990e0232ed5a459ff94eb38904
Alex Freire <alexfreirespinola@gmail.com>
2021-08-13 21:45:30 -0300

    feat: add new libraries and setup tests libraries added: jest, react-native-testing-library and react-native-responsive-dimensions

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-expo',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
+  testMatch: ['**/__tests__/?(*.)+(test).js'],
+};
