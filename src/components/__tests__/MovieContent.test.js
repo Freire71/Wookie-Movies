3fe7746f10ba76910e027f26430000b528ba446b
Alex Freire <alexfreirespinola@gmail.com>
2021-08-15 00:56:34 -0300

    test: add movie content component tests

---
+++ src/components/__tests__/MovieContent.test.js
@@ -0,0 +1,20 @@
+import 'react-native';
+import React from 'react';
+import { render } from '../../utils/Tests';
+import MovieContent from '../MovieContent';
+
+describe('<MovieContent />', () => {
+  it('should render properly ', () => {
+    const synopse = 'lorem ipsum dolorem';
+    const cast = ['Jose', 'Maria', 'Marco'];
+    const castDisplayedValue = cast.join(', ');
+    const { getByText } = render(
+      <MovieContent synopse={synopse} cast={cast} />
+    );
+    const synopseComponent = getByText(synopse);
+    const castComponent = getByText(castDisplayedValue);
+
+    expect(synopseComponent).toBeTruthy();
+    expect(castComponent).toBeTruthy();
+  });
+});
