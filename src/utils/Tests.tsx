b8efc453cce1e100a8492055c5eed18dc4c12a4b
Alex Freire <alexfreirespinola@gmail.com>
2021-08-17 17:08:42 -0300

    feat: add movies payload file

--- src/utils/Tests.tsx
+++ src/utils/Tests.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { ThemeProvider } from 'styled-components/native';
 import theme from '../config/Theme';
+import { movies } from './payloads/MoviesPayload';
 
 export function renderWithProvider({
   children,
@@ -14,7 +15,8 @@ export function renderWithProvider({
 const customRender = (ui: any) => render(ui, { wrapper: renderWithProvider });
 
 export * from '@testing-library/react-native';
-export { customRender as render };
+
+export { customRender as render, movies };
 
 export function testID(id: string) {
   return __DEV__ ? { testID: id } : {};
