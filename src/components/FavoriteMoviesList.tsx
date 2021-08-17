ead27f1d5313efb55c94aff9e9a012c456985b9f
Alex Freire <alexfreirespinola@gmail.com>
2021-08-16 23:43:46 -0300

    feat: add favorite movies list component

---
+++ src/components/FavoriteMoviesList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FlatList, ListRenderItemInfo } from 'react-native';
+import { Movie } from '../api/types/movie';
+import FavoriteMoviesListItem from './FavoriteMoviesListItem';
+import ListMessage from './ListMessage';
+
+interface IFavoriteMoviesListProps {
+  data: Movie[];
+  onMoviePress: (movie: Movie) => void;
+}
+
+const EMPTY_LIST_MESSAGE_TITLE =
+  'You do not have marked any movie as your favorite';
+const EMPTY_LIST_MESSAGE_SUBTITLE =
+  'To favorite a movie press the ♡ icon on the movie details page';
+
+const FavoriteMoviesList = ({
+  data,
+  onMoviePress,
+}: IFavoriteMoviesListProps) => {
+  const keyExtractor = (item: Movie, index: number) => `${item.id}/${index}`;
+
+  const renderItem = ({ item }: ListRenderItemInfo<Movie>) => {
+    return <FavoriteMoviesListItem movie={item} onMoviePress={onMoviePress} />;
+  };
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+      ListEmptyComponent={() => (
+        <ListMessage
+          title={EMPTY_LIST_MESSAGE_TITLE}
+          subtitle={EMPTY_LIST_MESSAGE_SUBTITLE}
+        />
+      )}
+    />
+  );
+};
+
+export default FavoriteMoviesList;
