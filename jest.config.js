c37c9b7cb81ac192ced8c79940053c9dffef8d51
Alex Freire <alexfreirespinola@gmail.com>
2021-08-17 13:01:43 -0300

    chore: add react native async storage jest mock

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: [
+    './src/config/jestSetup.js',
+    '@testing-library/jest-native/extend-expect',
+  ],
   testMatch: ['**/__tests__/?(*.)+(test).js'],
 };
