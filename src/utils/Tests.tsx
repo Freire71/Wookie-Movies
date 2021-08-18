f3a04bd3e0700f0d571835a29cb346d83dcddf55
Alex Freire <alexfreirespinola@gmail.com>
2021-08-17 22:51:24 -0300

    tests: add new methods to tests utils file

--- src/utils/Tests.tsx
+++ src/utils/Tests.tsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react-native';
 import { ThemeProvider } from 'styled-components/native';
 import theme from '../config/Theme';
 import { movies } from './payloads/MoviesPayload';
+import { QueryClient, QueryClientProvider, setLogger } from 'react-query';
 
 export function renderWithProvider({
   children,
@@ -12,11 +13,35 @@ export function renderWithProvider({
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
+export function renderWithThemeAndReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: false } },
+  });
+  setLogger({
+    log: console.log,
+    warn: console.warn,
+    error: () => {},
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
 const customRender = (ui: any) => render(ui, { wrapper: renderWithProvider });
+
+const pageRender = (ui: any) =>
+  render(ui, { wrapper: renderWithThemeAndReactQueryProvider });
 
 export * from '@testing-library/react-native';
 
-export { customRender as render, movies };
+export { customRender as render, movies, pageRender };
 
 export function testID(id: string) {
   return __DEV__ ? { testID: id } : {};
