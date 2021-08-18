23d66d6304bd3fad3b46923753723831c95bc8da
Alex Freire <alexfreirespinola@gmail.com>
2021-08-17 22:50:33 -0300

    chore: improve jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*|@react-native-community|@react-navigation)',
   ],
   setupFilesAfterEnv: [
     './src/config/SetupJest.js',
